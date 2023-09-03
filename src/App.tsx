2bf51de2e06481653955804a77abb66f87fa5490
YasserAbdalla <yasserabdalla149@gmail.com>
2023-09-03 07:41:20 +0300

    make project section

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Header from "./components/Header";
 import Skills from "./components/Skills";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Main from "./components/Main";
+import Projects from "./components/Projects";
 function App() {
   const [count, setCount] = useState(0);
   return (
     <>
       <Header />
       <Main />
+      <Projects />
       <Skills />
     </>
   );
